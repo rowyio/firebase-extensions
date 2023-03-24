5f06cd701c6aa1706d40c2903c9cbd80a57362b3
Yaman Katby <m.yaman.katby@gmail.com>
2023-03-24 15:48:00 +0300

    feat(auth-create-firestore-user-doc): add fields to populate param

--- auth-create-firestore-user-doc/functions/src/config.ts
+++ auth-create-firestore-user-doc/functions/src/config.ts
@@ -1,9 +1,11 @@
 interface IConfig {
   usersCollectionPath: string;
+  fieldsToPopulate: string[];
 }
 
 const config: IConfig = {
   usersCollectionPath: process.env.USERS_COLLECTION_PATH!,
+  fieldsToPopulate: process.env.FIELDS_TO_POPULATE!.split(','),
 };
 
 export default config;
--- auth-create-firestore-user-doc/functions/src/index.ts
+++ auth-create-firestore-user-doc/functions/src/index.ts
@@ -1,5 +1,6 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { UserRecord } from 'firebase-admin/auth';
+import * as functions from 'firebase-functions';
 import config from './config';
 
 admin.initializeApp();
@@ -11,13 +12,12 @@ const usersCollection = db.collection(config.usersCollectionPath);
 export const createUserDocument = functions.auth
   .user()
   .onCreate(async (user) => {
-    usersCollection.doc(user.uid).set({
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      phoneNumber: user.phoneNumber,
-      disabled: user.disabled,
-      lastSignInTime: user.metadata.lastSignInTime,
-      creationTime: user.metadata.creationTime,
-    });
+    const doc: any = {};
+    for (const field of config.fieldsToPopulate) {
+      const fieldValue = user[field as keyof UserRecord];
+      if (fieldValue) {
+        doc[field] = fieldValue;
+      }
+    }
+    return usersCollection.doc(user.uid).set(doc);
   });
