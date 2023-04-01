225de66aa1b9798571f8d71cddb1404704a2ba9a
Yaman Katby <m.yaman.katby@gmail.com>
2023-04-01 16:18:18 +0300

    feat(auth-create-firestore-user-doc): add backfilling function

--- auth-create-firestore-user-doc/functions/src/config.ts
+++ auth-create-firestore-user-doc/functions/src/config.ts
@@ -1,11 +1,15 @@
 interface IConfig {
   usersCollectionPath: string;
   fieldsToPopulate: string[];
+  backfillExistingUsers: boolean;
+  location: string;
 }
 
 const config: IConfig = {
   usersCollectionPath: process.env.USERS_COLLECTION_PATH!,
   fieldsToPopulate: process.env.FIELDS_TO_POPULATE!.split(','),
+  backfillExistingUsers: process.env.BACKFILL_EXISTING_USERS === 'true',
+  location: process.env.FUNCTIONS_LOCATION!,
 };
 
 export default config;
