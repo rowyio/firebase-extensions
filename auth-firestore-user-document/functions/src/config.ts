460879f34ffaedf4d693708f4f971c93af0e5d81
Yaman Katby <m.yaman.katby@gmail.com>
2023-04-02 00:42:53 +0300

    feat: add deleteUserDocument function

--- auth-firestore-user-document/functions/src/config.ts
+++ auth-firestore-user-document/functions/src/config.ts
@@ -2,6 +2,7 @@ interface IConfig {
   usersCollectionPath: string;
   fieldsToPopulate: string[];
   backfillExistingUsers: boolean;
+  deleteDocumentOnUserDelete: boolean;
   location: string;
 }
 
@@ -9,6 +10,8 @@ const config: IConfig = {
   usersCollectionPath: process.env.USERS_COLLECTION_PATH!,
   fieldsToPopulate: process.env.FIELDS_TO_POPULATE!.split(','),
   backfillExistingUsers: process.env.BACKFILL_EXISTING_USERS === 'true',
+  deleteDocumentOnUserDelete:
+    process.env.DELETE_DOCUMENT_ON_USER_DELETE === 'true',
   location: process.env.FUNCTIONS_LOCATION!,
 };
 
